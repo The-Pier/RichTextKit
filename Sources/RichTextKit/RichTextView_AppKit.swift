939ef3a54c4bcf1507e54053de5a2942d7dba595
adrixdax <47791835+adrixdax@users.noreply.github.com>
2023-10-17 14:43:25 +0200

    missing return statement

--- Sources/RichTextKit/RichTextView_AppKit.swift
+++ Sources/RichTextKit/RichTextView_AppKit.swift
@@ -48,7 +48,7 @@ open class RichTextView: NSTextView, RichTextViewComponent {
                 let fileExtension = url.pathExtension.lowercased()
                 let imageExtensions = ["jpg", "jpeg", "png", "gif", "tiff", "bmp", "heic"]
                 if imageExtensions.contains(fileExtension) {
-                    return image
+                    return pasteImage(image, at: selectedRange.location)
                 }
             }
             return nil
